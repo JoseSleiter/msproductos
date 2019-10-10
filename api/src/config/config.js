b342ae24d53db84c074de1413758c9ca6e762365
Jose Sleiter Rios <josesleiter@gmail.com>
2019-10-10 04:01:50 -0400

    feat: finished module

--- api/src/config/config.js
+++ api/src/config/config.js
@@ -21,7 +21,7 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev1'
 /**
  * Host de la Base de Datos
  */
-let URL_MONGODB =  "mongodb+srv://usertest:js123456@mscarrito-fr90b.mongodb.net/test?retryWrites=true&w=majority"
+let URL_MONGODB =  "mongodb+srv://usertest:js123456@mscarrito-fr90b.mongodb.net/productos?retryWrites=true&w=majority"
 process.env.DATABASE_URL = process.env.NODE_ENV === 'dev'? "mongodb://localhost:27017/local" : URL_MONGODB
 
 // process.env.DATABASE_URL = 'mongodb://'+process.env.MONGO_SERVICE_HOST+':'+process.env.MONGO_SERVICE_PORT+'/test';
